d9d924293865aaec342015d366240de1ef11fcc4
Vaibhav Kambar <vaibhavkambar123@gmail.com>
2022-01-31 13:36:47 +0530

    Initial commit

--- app/src/main/java/com/example/newsdaily/NewsListAdapter.kt
+++ app/src/main/java/com/example/newsdaily/NewsListAdapter.kt
@@ -6,7 +6,7 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-class NewsListAdapter(private val items: ArrayList<String>,private val listener:NewsItemClicked): RecyclerView.Adapter<NewsViewHolder>() {//adapter ko data milega uske constructor ke andar
+class NewsListAdapter(private val items: ArrayList<String>,private val listener:NewsItemClicked): RecyclerView.Adapter<NewsViewHolder>() {
 override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NewsViewHolder {
     val view = LayoutInflater.from(parent.context).inflate(R.layout.items_news,parent,false)
     val viewHolder=NewsViewHolder(view)
